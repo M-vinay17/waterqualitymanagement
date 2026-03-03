ad56d431d6e15d3395bd7e6241195be34649be15
sneha srivastava <snehasrivastava21.2003@gmail.com>
2026-03-03 16:06:35 +0530

    Milestone 2: Base map implementation using React Leaflet

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from app.routes import user, auth
-from app.core.database import engine, Base
-from app.models import user as user_model
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.routes import user, auth, water
+from app.core.database import engine, Base
+
+from app.models import user as user_model
+from app.models import water_reading
 
 app = FastAPI()
 
@@ -12,16 +15,21 @@ Base.metadata.create_all(bind=engine)
 # Include routes
 app.include_router(auth.router)
 app.include_router(user.router)
+app.include_router(water.router)
 
-
+# CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React default
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Root endpoint
 @app.get("/")
 def home():
-    return {"message": "✅ Backend is running successfully!"}
+    return {"message": "Backend is running successfully!"}
